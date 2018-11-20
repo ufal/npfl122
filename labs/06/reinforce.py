fa80e172c682b99ce82d487ff7812b6d36134448
Milan Straka <milan@strakovi.com>
2018-11-20 05:56:20 +0100

    Add the reinforce task.

---
+++ labs/06/reinforce.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+import numpy as np
+import tensorflow as tf
+
+import cart_pole_evaluator
+
+class Network:
+    def __init__(self, threads, seed=42):
+        # Create an empty graph and a session
+        graph = tf.Graph()
+        graph.seed = seed
+        self.session = tf.Session(graph = graph, config=tf.ConfigProto(inter_op_parallelism_threads=threads,
+                                                                       intra_op_parallelism_threads=threads))
+
+    def construct(self, args, state_shape, num_actions):
+        with self.session.graph.as_default():
+            self.states = tf.placeholder(tf.float32, [None] + state_shape)
+            self.actions = tf.placeholder(tf.int32, [None])
+            self.returns = tf.placeholder(tf.float32, [None])
+
+            # TODO: Start with self.states and
+            # - add a fully connected layer of size args.hidden_layer and ReLU activation
+            # - add a fully connected layer with num_actions and no activation, computing `logits`
+            # - compute `self.probabilities` as tf.nn.softmax of `logits`
+
+            # TODO: Training
+            # - compute `loss` as sparse softmax cross entropy of `self.actions` and `logits`,
+            # weighted by `self.returns` (using `weights` param)
+
+            global_step = tf.train.create_global_step()
+            self.training = tf.train.AdamOptimizer(args.learning_rate).minimize(loss, global_step=global_step, name="training")
+
+            # Initialize variables
+            self.session.run(tf.global_variables_initializer())
+
+    def predict(self, states):
+        return self.session.run(self.probabilities, {self.states: states})
+
+    def train(self, states, actions, returns):
+        self.session.run(self.training, {self.states: states, self.actions: actions, self.returns: returns})
+
+if __name__ == "__main__":
+    # Fix random seed
+    np.random.seed(42)
+
+    # Parse arguments
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--batch_size", default=None, type=int, help="Number of episodes to train on.")
+    parser.add_argument("--episodes", default=None, type=int, help="Training episodes.")
+    parser.add_argument("--gamma", default=1.0, type=float, help="Discounting factor.")
+    parser.add_argument("--hidden_layer", default=20, type=int, help="Size of hidden layer.")
+    parser.add_argument("--learning_rate", default=None, type=float, help="Learning rate.")
+    parser.add_argument("--render_each", default=0, type=int, help="Render some episodes.")
+    parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
+    args = parser.parse_args()
+
+    # Create the environment
+    env = cart_pole_evaluator.environment(discrete=False)
+
+    # Construct the network
+    network = Network(threads=args.threads)
+    network.construct(args, env.state_shape, env.actions)
+
+    # Training
+    for _ in range(args.episodes // args.batch_size):
+        batch_states, batch_actions, batch_returns = [], [], []
+        for _ in range(args.batch_size):
+            # Perform episode
+            states, actions, rewards = [], [], []
+            state, done = env.reset(), False
+            while not done:
+                if args.render_each and env.episode > 0 and env.episode % args.render_each == 0:
+                    env.render()
+
+                # TODO: Compute action probabilities using `network.predict` and current `state`
+
+                # TODO: Choose `action` according to `probabilities` distribution (np.random.choice can be used)
+
+                next_state, reward, done, _ = env.step(action)
+
+                states.append(state)
+                actions.append(action)
+                rewards.append(reward)
+
+                state = next_state
+
+            # TODO: Compute returns by summing rewards (with discounting)
+
+            # TODO: Add states, actions and returns to the training batch
+
+        # Train using the generated batch
+        network.train(batch_states, batch_actions, batch_returns)
+
+    # Final evaluation
+    while True:
+        state, done = env.reset(True), False
+        while not done:
+            probabilities = network.predict([state])[0]
+            action = np.argmax(probabilities)
+            state, reward, done, _ = env.step(action)
--- lectures/lecture06.md
+++ lectures/lecture06.md
@@ -1,3 +1,4 @@
 ### Lecture: 6. Rainbow, Policy Gradient Methods
 #### Date: Nov 19
 #### Slides: https://ufal.mff.cuni.cz/~straka/courses/npfl122/1819/slides/?06
+#### Lecture assignment: reinforce
