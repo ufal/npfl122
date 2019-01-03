0a231c7f5d8c5928aae25bc1f5af3c7df24e66d9
Milan Straka <milan@strakovi.com>
2019-01-03 10:47:32 +0100

    Fix typo.

--- labs/10/az_quiz.py
+++ labs/10/az_quiz.py
@@ -30,7 +30,7 @@ class AZQuiz:
 
     @property
     def to_play(self):
-        return self._to_play if self._winner is None nelse None
+        return self._to_play if self._winner is None else None
 
     @property
     def winner(self):
