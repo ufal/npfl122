b79717c26a1aea7eca7db547fdb6a7b8115e505b
Milan Straka <milan@strakovi.com>
2020-10-21 07:33:39 +0200

    Update wrappers.

--- labs/01/wrappers.py
+++ labs/01/wrappers.py
@@ -56,6 +56,7 @@ class EvaluationWrapper(gym.Wrapper):
 
         return observation, reward, done, info
 
+
 class DiscretizationWrapper(gym.ObservationWrapper):
     def __init__(self, env, separators):
         super().__init__(env)
@@ -73,6 +74,7 @@ class DiscretizationWrapper(gym.ObservationWrapper):
             state += np.digitize(observation, separator)
         return state
 
+
 class DiscreteCartPoleWrapper(DiscretizationWrapper):
     def __init__(self, env, bins=8):
         super().__init__(env, [
@@ -81,3 +83,45 @@ class DiscreteCartPoleWrapper(DiscretizationWrapper):
             np.linspace(-0.5, 0.5, num=bins + 1)[1:-1], # cart velocity
             np.linspace(-2, 2, num=bins + 1)[1:-1],     # pole angle velocity
         ])
+
+
+class DiscreteMountainCarWrapper(DiscretizationWrapper):
+    def __init__(self, env, bins=24):
+        super().__init__(env, [
+            np.linspace(-1.2, 0.6, num=bins + 1)[1:-1],   # car position
+            np.linspace(-0.07, 0.07, num=bins + 1)[1:-1], # car velocity
+        ])
+
+
+class DiscreteLunarLanderWrapper(DiscretizationWrapper):
+    def __init__(self, env):
+        super().__init__(env, [
+            np.linspace(-.4,   .4, num=5 + 1)[1:-1],   # x
+            np.linspace(-.075,1.35,num=6 + 1)[1:-1],   # y
+            np.linspace(-.5,   .5, num=5 + 1)[1:-1],   # vel x
+            np.linspace(-.8,   .8, num=7 + 1)[1:-1],   # vel y
+            np.linspace(-.2,   .2, num=3 + 1)[1:-1],   # rot
+            np.linspace(-.2,   .2, num=5 + 1)[1:-1],   # ang vel
+            [.5], #lc
+            [.5], #rc
+        ])
+
+        self._expert = gym.make("LunarLander-v2")
+        self._expert.seed(42)
+
+    def expert_trajectory(self):
+        state, trajectory, done = self._expert.reset(), [], False
+        initial_state = self.observation(state)
+        while not done:
+            action = gym.envs.box2d.lunar_lander.heuristic(self._expert, state)
+            state, reward, done, _ = self._expert.step(action)
+            trajectory.append((action, reward, self.observation(state)))
+        return initial_state, trajectory
+
+
+gym.envs.register(
+    id="MountainCar1000-v0",
+    entry_point="gym.envs.classic_control:MountainCarEnv",
+    max_episode_steps=1000,
+    reward_threshold=-110.0,
+)
