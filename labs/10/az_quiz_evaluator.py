e23ac699871bb09083ebce5f1081f4eacf055e18
Milan Straka <milan@strakovi.com>
2020-12-20 21:53:58 +0100

    Add --first_fixed, remove exception handling.
    It is fine to fail on exception.

--- labs/10/az_quiz_evaluator.py
+++ labs/10/az_quiz_evaluator.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import argparse
 import importlib
-import sys
 import time
 
 import az_quiz
@@ -15,32 +14,32 @@ def load_player(player):
     args.recodex = True
     return module.main(args)
 
-def evaluate(players, games, randomized, render):
+def evaluate(players, games, randomized, first_fixed, render):
     wins = [0, 0]
     for i in range(games):
         for to_start in range(2):
             game = az_quiz.AZQuiz(randomized)
-            try:
-                while game.winner is None:
-                    game.move(players[to_start ^ game.to_play].play(game.clone()))
-                    if render:
-                        game.render()
-                        time.sleep(0.3)
-            except ValueError:
-                pass
+            if first_fixed:
+                game.move(i % game.actions)
+            while game.winner is None:
+                game.move(players[to_start ^ game.to_play].play(game.clone()))
+                if render:
+                    game.render()
+                    time.sleep(0.3)
             if game.winner == to_start:
                 wins[to_start] += 1
             if render:
                 time.sleep(1.0)
 
         print("First player win rate after {} games: {:.2f}% ({:.2f}% and {:.2f}% when starting and not starting)".format(
-            2 * i + 2, 100 * (wins[0] + wins[1]) / (2 * i + 2), 100 * wins[0] / (i + 1), 100 * wins[1] / (i + 1)), file=sys.stderr)
+            2 * i + 2, 100 * (wins[0] + wins[1]) / (2 * i + 2), 100 * wins[0] / (i + 1), 100 * wins[1] / (i + 1)))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("player_1", type=str, help="First player module")
     parser.add_argument("player_2", type=str, help="Second player module")
+    parser.add_argument("--first_fixed", default=False, action="store_true", help="Choose first move randomly")
     parser.add_argument("--games", default=56, type=int, help="Number of alternating games to evaluate")
     parser.add_argument("--randomized", default=False, action="store_true", help="Is answering allowed to fail and generate random results")
     parser.add_argument("--render", default=False, action="store_true", help="Should the games be rendered")
@@ -50,5 +49,6 @@ if __name__ == "__main__":
         [load_player(args.player_1), load_player(args.player_2)],
         games=args.games,
         randomized=args.randomized,
+        first_fixed=args.first_fixed,
         render=args.render,
     )
