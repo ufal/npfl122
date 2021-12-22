aac05d543851a22e5e598d3f726a9891be87f510
Milan Straka <milan@strakovi.com>
2021-12-22 13:23:52 +0100

    Finalize template for memory_game_rl.

--- labs/12/memory_game_rl.py
+++ labs/12/memory_game_rl.py
@@ -20,6 +20,7 @@ parser.add_argument("--seed", default=None, type=int, help="Random seed.")
 parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
 # If you add more arguments, ReCodEx will keep them with your default values.
 parser.add_argument("--batch_size", default=None, type=int, help="Number of episodes to train on.")
+parser.add_argument("--gradient_clipping", default=None, type=float, help="Gradient clipping.")
 parser.add_argument("--entropy_regularization", default=None, type=float, help="Entropy regularization weight.")
 parser.add_argument("--evaluate_each", default=None, type=int, help="Evaluate each number of episodes.")
 parser.add_argument("--evaluate_for", default=None, type=int, help="Evaluate for number of episodes.")
@@ -29,23 +30,23 @@ parser.add_argument("--memory_cell_size", default=None, type=int, help="Memory c
 parser.add_argument("--replay_buffer", default=None, type=int, help="Max replay buffer size; default batch_size")
 
 class Network:
-    def __init__(self, env, args):
+    def __init__(self, env: wrappers.EvaluationEnv, args: argparse.Namespace) -> None:
         self.args = args
         self.env = env
 
-        # Define the agent inputs: a memory, and a state.
+        # Define the agent inputs: a memory and a state.
         memory = tf.keras.layers.Input(shape=[args.memory_cells, args.memory_cell_size], dtype=tf.float32)
         state = tf.keras.layers.Input(shape=env.observation_space.shape, dtype=tf.int32)
 
         # Encode the input state, which is a (card, observation) pair,
         # by representing each element as one-hot and concatenating them, resulting
-        # in a vector of length sum(env.observation_space.nvec).
+        # in a vector of length `sum(env.observation_space.nvec)`.
         encoded_input = tf.keras.layers.Concatenate()(
             [tf.one_hot(state[:, i], dim) for i, dim in enumerate(env.observation_space.nvec)])
 
         # TODO(memory_game): Generate a read key for memory read from the encoded input, by using
         # a ReLU hidden layer of size `args.hidden_layer` followed by a dense layer
-        # with `args.memory_cell_size` units and tanh activation (to keep the memory
+        # with `args.memory_cell_size` units and `tanh` activation (to keep the memory
         # content in limited range).
 
         # TODO(memory_game): Read the memory using the generated read key. Notably, compute cosine
@@ -55,7 +56,7 @@ class Network:
 
         # TODO(memory_game): Using concatenated encoded input and the read value, use a ReLU hidden
         # layer of size `args.hidden_layer` followed by a dense layer with
-        # `env.action_space.n` units and softmax activation to produce a policy.
+        # `env.action_space.n` units and `softmax` activation to produce a policy.
 
         # TODO(memory_game): Perform memory write. For faster convergence, append directly
         # the `encoded_input` to the memory, i.e., add it as a first memory row, and drop
@@ -63,14 +64,14 @@ class Network:
 
         # Create the agent
         self._agent = tf.keras.Model(inputs=[memory, state], outputs=[updated_memory, policy])
-
-        # Prepare an optimizer with the default learning rate and a loss
-        self._optimizer = tf.optimizers.Adam()
-        self._loss = tf.losses.SparseCategoricalCrossentropy()
+        self._agent.compile(
+            optimizer=tf.optimizers.Adam(clipnorm=args.gradient_clipping),
+            loss=tf.losses.SparseCategoricalCrossentropy(),
+        )
 
     def zero_memory(self):
         # TODO(memory_game): Return an empty memory. It should be a TF tensor
-        # with shape (self.args.memory_cells, self.args.memory_cell_size).
+        # with shape `[self.args.memory_cells, self.args.memory_cell_size]`.
         raise NotImplementedError()
 
     @wrappers.typed_np_function(np.float32, np.int32, np.float32)
@@ -78,12 +79,17 @@ class Network:
     def _train(self, states, actions, returns):
         # TODO: Train the network given a batch of sequences of `states`
         # (each being a (card, symbol) pair), sampled `actions` and observed `returns`.
-        #
         # Specifically, start with a batch of empty memories, and run the agent
         # sequentially as many times as necessary, using `actions` as actions.
         #
-        # Note that the sequences can be of different length, so you have to
-        # provide a way of padding the episodes.
+        # Use the REINFORCE algorithm, optionally with a baseline. Note that
+        # I use a baseline, but not a baseline computed by a neural network;
+        # instead, for every time step, I track exponential moving average of
+        # observed returns, with momentum 0.01.
+        #
+        # Note that the sequences can be of different length, so you need to pad them
+        # to same length and then somehow indicate the length of the individual episodes
+        # (one possibility is to add another parameter to `_train`).
         raise NotImplementedError()
 
     def train(self, episodes):
@@ -104,7 +110,7 @@ def main(env, args):
     tf.config.threading.set_inter_op_parallelism_threads(args.threads)
     tf.config.threading.set_intra_op_parallelism_threads(args.threads)
 
-    # Post-process arguments
+    # Post-process arguments to default values if not overridden on the command line.
     if args.hidden_layer is None:
         args.hidden_layer = 8 * args.cards
     if args.memory_cells is None:
@@ -125,7 +131,7 @@ def main(env, args):
             if args.render_each and env.episode > 0 and env.episode % args.render_each == 0:
                 env.render()
 
-            # TODO: Find out which action to use
+            # TODO(memory_game): Find out which action to use
             action = None
 
             state, reward, done, _ = env.step(action)
@@ -155,7 +161,7 @@ def main(env, args):
             if len(replay_buffer) >= args.batch_size:
                 network.train([replay_buffer[i] for i in np.random.choice(len(replay_buffer), size=args.batch_size, replace=False)])
 
-        # TODO: Maybe evaluate the current performance, using
+        # TODO(memory_game): Maybe evaluate the current performance, using
         # `evaluate_episode()` method returning the achieved return,
         # and setting `training=False` when the performance is high enough.
 
@@ -168,6 +174,6 @@ if __name__ == "__main__":
     args = parser.parse_args([] if "__file__" not in globals() else None)
 
     # Create the environment
-    env = wrappers.EvaluationWrapper(memory_game_environment.make(args.cards), args.seed, evaluate_for=args.evaluate_for, report_each=args.evaluate_for)
+    env = wrappers.EvaluationEnv(memory_game_environment.make(args.cards), args.seed, evaluate_for=args.evaluate_for, report_each=args.evaluate_for)
 
     main(env, args)
