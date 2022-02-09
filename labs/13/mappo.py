0a54f2de2dde55d24640b204e7290b12d01cb2d9
Milan Straka <milan@strakovi.com>
2022-02-09 22:37:41 +0100

    Specify 2 agents.

--- labs/13/mappo.py
+++ labs/13/mappo.py
@@ -12,7 +12,7 @@ import wrappers
 
 parser = argparse.ArgumentParser()
 # These arguments will be set appropriately by ReCodEx, even if you change them.
-parser.add_argument("--agents", default=None, type=int, help="Agents to use.")
+parser.add_argument("--agents", default=2, type=int, help="Agents to use.")
 parser.add_argument("--recodex", default=False, action="store_true", help="Running in ReCodEx")
 parser.add_argument("--render_each", default=0, type=int, help="Render some episodes.")
 parser.add_argument("--seed", default=None, type=int, help="Random seed.")
