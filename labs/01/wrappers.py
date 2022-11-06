ufal/npfl122

commit dfc6891710ccf8ab9dc38f5d2361a5fc69fad120

The observation of MultiDiscrete space should be a numpy array.

labs/01/wrappers.py
@@ -106,7 +106,8 @@ class DiscretizationWrapper(gym.ObservationWrapper):
         if self._tiles is None:
             return state
         else:
-            states = [state]
+            states = np.empty(self._tiles, dtype=np.int64)
+            states[0] = state
             for t in range(1, self._tiles):
                 state = 0
                 for i in range(len(self._separators)):
@@ -116,7 +117,7 @@ class DiscretizationWrapper(gym.ObservationWrapper):
                         state += 1 + len(self._separators[i])
                     else:
                         state += np.digitize(value, self._separators[i])
-                states.append(self._first_tile_states + (t - 1) * self._rest_tiles_states + state)
+                states[t] = state
             return states
 
 

labs/03/wrappers.py
@@ -106,7 +106,8 @@ class DiscretizationWrapper(gym.ObservationWrapper):
         if self._tiles is None:
             return state
         else:
-            states = [state]
+            states = np.empty(self._tiles, dtype=np.int64)
+            states[0] = state
             for t in range(1, self._tiles):
                 state = 0
                 for i in range(len(self._separators)):
@@ -116,7 +117,7 @@ class DiscretizationWrapper(gym.ObservationWrapper):
                         state += 1 + len(self._separators[i])
                     else:
                         state += np.digitize(value, self._separators[i])
-                states.append(self._first_tile_states + (t - 1) * self._rest_tiles_states + state)
+                states[t] = state
             return states
 
 

labs/04/wrappers.py
@@ -106,7 +106,8 @@ class DiscretizationWrapper(gym.ObservationWrapper):
         if self._tiles is None:
             return state
         else:
-            states = [state]
+            states = np.empty(self._tiles, dtype=np.int64)
+            states[0] = state
             for t in range(1, self._tiles):
                 state = 0
                 for i in range(len(self._separators)):
@@ -116,7 +117,7 @@ class DiscretizationWrapper(gym.ObservationWrapper):
                         state += 1 + len(self._separators[i])
                     else:
                         state += np.digitize(value, self._separators[i])
-                states.append(self._first_tile_states + (t - 1) * self._rest_tiles_states + state)
+                states[t] = state
             return states
 
 

labs/05/wrappers.py
@@ -106,7 +106,8 @@ class DiscretizationWrapper(gym.ObservationWrapper):
         if self._tiles is None:
             return state
         else:
-            states = [state]
+            states = np.empty(self._tiles, dtype=np.int64)
+            states[0] = state
             for t in range(1, self._tiles):
                 state = 0
                 for i in range(len(self._separators)):
@@ -116,7 +117,7 @@ class DiscretizationWrapper(gym.ObservationWrapper):
                         state += 1 + len(self._separators[i])
                     else:
                         state += np.digitize(value, self._separators[i])
-                states.append(self._first_tile_states + (t - 1) * self._rest_tiles_states + state)
+                states[t] = state
             return states
 
 

commit 156899354fac808f8a535a510e29ee065de5ff91

Fix the error in the tile encoding introduced in the commit dfc6891.

labs/01/wrappers.py
@@ -117,7 +117,7 @@ class DiscretizationWrapper(gym.ObservationWrapper):
                         state += 1 + len(self._separators[i])
                     else:
                         state += np.digitize(value, self._separators[i])
-                states[t] = state
+                states[t] = self._first_tile_states + (t - 1) * self._rest_tiles_states + state
             return states
 
 

labs/03/wrappers.py
@@ -117,7 +117,7 @@ class DiscretizationWrapper(gym.ObservationWrapper):
                         state += 1 + len(self._separators[i])
                     else:
                         state += np.digitize(value, self._separators[i])
-                states[t] = state
+                states[t] = self._first_tile_states + (t - 1) * self._rest_tiles_states + state
             return states
 
 

labs/04/wrappers.py
@@ -117,7 +117,7 @@ class DiscretizationWrapper(gym.ObservationWrapper):
                         state += 1 + len(self._separators[i])
                     else:
                         state += np.digitize(value, self._separators[i])
-                states[t] = state
+                states[t] = self._first_tile_states + (t - 1) * self._rest_tiles_states + state
             return states
 
 

labs/05/wrappers.py
@@ -117,7 +117,7 @@ class DiscretizationWrapper(gym.ObservationWrapper):
                         state += 1 + len(self._separators[i])
                     else:
                         state += np.digitize(value, self._separators[i])
-                states[t] = state
+                states[t] = self._first_tile_states + (t - 1) * self._rest_tiles_states + state
             return states
 
 