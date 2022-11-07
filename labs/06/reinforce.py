3c848c223c5b0310d524edc08885a0d003c40dc1
Milan Straka <milan@strakovi.com>
2022-11-07 16:57:31 +0100

    Add lecture 6 assignments.
    However, the cart_pole_pixels_environment is still missing.

---
+++ labs/06/reinforce.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+import argparse
+import os
+os.environ.setdefault("TF_CPP_MIN_LOG_LEVEL", "2")  # Report only TF errors by default
+
+import gym
+import numpy as np
+import tensorflow as tf
+
+import wrappers
+
+parser = argparse.ArgumentParser()
+# These arguments will be set appropriately by ReCodEx, even if you change them.
+parser.add_argument("--recodex", default=False, action="store_true", help="Running in ReCodEx")
+parser.add_argument("--render_each", default=0, type=int, help="Render some episodes.")
+parser.add_argument("--seed", default=None, type=int, help="Random seed.")
+parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
+# For these and any other arguments you add, ReCodEx will keep your default value.
+parser.add_argument("--batch_size", default=None, type=int, help="Batch size.")
+parser.add_argument("--episodes", default=None, type=int, help="Training episodes.")
+parser.add_argument("--gamma", default=None, type=float, help="Discounting factor.")
+parser.add_argument("--hidden_layer_size", default=None, type=int, help="Size of hidden layer.")
+parser.add_argument("--learning_rate", default=None, type=float, help="Learning rate.")
+
+
+class Network:
+    def __init__(self, env: wrappers.EvaluationEnv, args: argparse.Namespace) -> None:
+        # TODO: Create a suitable model. The predict method assumes
+        # it is stored as `self._model`.
+        #
+        # Using Adam optimizer with given `args.learning_rate` is a good default.
+        raise NotImplementedError()
+
+    # TODO: Define a training method.
+    #
+    # Note that we need to use @tf.function for efficiency (using `train_on_batch`
+    # on extremely small batches/networks has considerable overhead).
+    #
+    # The `wrappers.typed_np_function` automatically converts input arguments
+    # to NumPy arrays of given type, and converts the result to a NumPy array.
+    @wrappers.typed_np_function(np.float32, np.int32, np.float32)
+    @tf.function(experimental_relax_shapes=True)
+    def train(self, states: np.ndarray, actions: np.ndarray, returns: np.ndarray) -> None:
+        raise NotImplementedError()
+
+    # Predict method, again with manual @tf.function for efficiency.
+    @wrappers.typed_np_function(np.float32)
+    @tf.function
+    def predict(self, states: np.ndarray) -> np.ndarray:
+        return self._model(states)
+
+
+def main(env: wrappers.EvaluationEnv, args: argparse.Namespace) -> None:
+    # Set random seeds and number of threads
+    if args.seed is not None:
+        tf.keras.utils.set_random_seed(args.seed)
+    tf.config.threading.set_inter_op_parallelism_threads(args.threads)
+    tf.config.threading.set_intra_op_parallelism_threads(args.threads)
+
+    # Construct the network
+    network = Network(env, args)
+
+    # Training
+    for _ in range(args.episodes // args.batch_size):
+        batch_states, batch_actions, batch_returns = [], [], []
+        for _ in range(args.batch_size):
+            # Perform episode
+            states, actions, rewards = [], [], []
+            state, done = env.reset()[0], False
+            while not done:
+                # TODO: Choose `action` according to probabilities
+                # distribution (see `np.random.choice`), which you
+                # can compute using `network.predict` and current `state`.
+                action = ...
+
+                next_state, reward, terminated, truncated, _ = env.step(action)
+                done = terminated or truncated
+
+                states.append(state)
+                actions.append(action)
+                rewards.append(reward)
+
+                state = next_state
+
+            # TODO: Compute returns by summing rewards (with discounting)
+
+            # TODO: Add states, actions and returns to the training batch
+
+        # TODO: Train using the generated batch.
+
+    # Final evaluation
+    while True:
+        state, done = env.reset(start_evaluation=True)[0], False
+        while not done:
+            # TODO: Choose a greedy action
+            action = ...
+            state, reward, terminated, truncated, _ = env.step(action)
+            done = terminated or truncated
+
+
+if __name__ == "__main__":
+    args = parser.parse_args([] if "__file__" not in globals() else None)
+
+    # Create the environment
+    env = wrappers.EvaluationEnv(gym.make("CartPole-v1"), args.seed, args.render_each)
+
+    main(env, args)
--- lectures/lecture06.md
+++ lectures/lecture06.md
@@ -4,6 +4,9 @@
 #### Reading: https://ufal.mff.cuni.cz/~straka/courses/npfl122/2223/slides.pdf/npfl122-06.pdf,PDF Slides
 #### Video: https://lectures.ms.mff.cuni.cz/video/rec/npfl122/2223/npfl122-06.mp4, Lecture
 #### Questions: #lecture_6_questions
+#### Lecture assignment: reinforce
+#### Lecture assignment: reinforce_baseline
+#### Lecture assignment: cart_pole_pixels
 
 - Policy Gradient Methods [Sections 13-13.1 of RLB]
 - Policy Gradient Theorem [Section 13.2 of RLB]
